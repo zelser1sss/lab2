ac9cd1a8867c4f16247138f13f7fbb7f5f020a74
dusta <dusta@DESKTOP-ORJ6J08>
2025-09-27 02:02:35 +0300

    Логику сохранения и загрузки адаптировал под map, также добавил возможность задавать имя файла для загрузки пользователю

--- Belkin_lab2/Belkin_lab2.cpp
+++ Belkin_lab2/Belkin_lab2.cpp
@@ -3,6 +3,8 @@
 #include <limits>
 #include <clocale>
 #include <map>
+#include <vector>
+#include <fstream>
 using namespace std;
 
 class Pipe 
@@ -165,6 +167,130 @@ void ViewAllObjects(const map<int, Pipe>& pipe_list, const map<int, CS>& cs_list
     };
 };
 
+void Save(const map<int, Pipe>& pipe_list, const map<int, CS>& cs_list)
+{
+    string file;
+    cout << "Введите название файла: ";
+    cin >> file;
+
+    ofstream save;
+    save.open(file);
+    if (save.is_open()) {
+        if (!(pipe_list.empty())) {
+            save << "ТРУБЫ\n";
+            for (const auto& element : pipe_list) {
+                save << element.first << "|" << element.second.name << "|" << element.second.length << "|" << element.second.diameter << "|" << element.second.repair << "|" << endl;
+            };
+            cout << "\nСписок труб сохранен!\n";
+        }
+        else {
+            save << "ТРУБ НЕТ\n";
+            cout << "\nТРУБ НЕТ\n";
+        };
+
+        if (!(cs_list.empty())) {
+            save << "КС\n";
+            for (const auto& element : cs_list) {
+                save << element.first << "|" << element.second.name << "|" << element.second.k_cex << "|" << element.second.k_cex_in_work << "|" << element.second.type << "|" << endl;
+            };
+            cout << "Список КС сохранен!\n\n";
+        }
+        else {
+            save << "КС НЕТ";
+            cout << "КС НЕТ\n\n";
+        };
+    }
+    else {
+        cout << "Недопустимое имя файла!\n\n";
+        return;
+    };
+};
+
+void Upload(map<int, Pipe>& pipe_list, map<int, CS>& cs_list)
+{
+    string file;
+    cout << "Введите имя файла: ";
+    cin >> file;
+
+    string line;
+    string state = "None";
+    ifstream upload(file);
+
+    if (!(upload.is_open())) {
+        cout << "\nФайл '" << file << "' не найден\n";
+        upload.close();
+        return;
+    };
+
+    pipe_list.clear();
+    cs_list.clear();
+
+    while (getline(upload, line)) {
+
+        if (line.empty() || line.find_first_not_of(' ') == string::npos) {
+            continue;
+        };
+
+        if ("ТРУБ НЕТ" == line) {
+            cout << "\nТРУБ НЕТ\n";
+            continue;
+        }
+        else if ("ТРУБЫ" == line) {
+            state = "Trubs";
+            continue;
+        };
+
+        if ("КС НЕТ" == line) {
+            cout << "КС НЕТ\n\n";
+            continue;
+        }
+        else if ("КС" == line) {
+            state = "CS";
+            continue;
+        };
+
+        vector<size_t> pos = { 0 };
+        for (size_t i = 0; i < 5; i++) {
+            size_t found_pos = line.find('|', pos[i]);
+            pos.push_back(found_pos + 1);
+        };
+
+        vector<string> sub;
+        for (size_t i = 0; i < (pos.size() - 1); i++) {
+            string found_sub = line.substr(pos[i], pos[i + 1] - pos[i] - 1);
+            sub.push_back(found_sub);
+        };
+
+        if (state == "Trubs") {
+            int ID = stoi(sub[0]);
+            Pipe newPipe;
+            newPipe.name = sub[1];
+            newPipe.length = stof(sub[2]);
+            newPipe.diameter = stoi(sub[3]);
+            if (stoi(sub[4]) == 1) {
+                newPipe.repair = true;
+            }
+            else {
+                newPipe.repair = false;
+            };
+
+            pipe_list.emplace(ID, newPipe);
+        };
+
+        if (state == "CS") {
+            int ID = stoi(sub[0]);
+            CS newCS;
+            newCS.name = sub[1];
+            newCS.k_cex = stoi(sub[2]);
+            newCS.k_cex_in_work = stoi(sub[3]);
+            newCS.type = sub[4];
+
+            cs_list.emplace(ID, newCS);
+        };
+    };
+    upload.close();
+};
+
 void Menu(map<int, Pipe>& pipe_list, map<int, CS>& cs_list)
 {
     while (1) {
@@ -190,8 +316,10 @@ void Menu(map<int, Pipe>& pipe_list, map<int, CS>& cs_list)
         case 5:
             break;
         case 6:
+            Save(pipe_list, cs_list);
             break;
         case 7:
+            Upload(pipe_list, cs_list);
             break;
         case 8:
             cout << "\nВыходим...\n";
